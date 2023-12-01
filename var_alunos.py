d65a467845c49158672f82846f7967403526976f
soph <106696912+sophluv@users.noreply.github.com>
2023-12-01 23:30:19 +0000

    Update var_alunos.py

--- var_alunos.py
+++ var_alunos.py
@@ -1,52 +1,49 @@
 import foosball_alunos
 
 
-"""
-Função que recebe o nome de um ficheiro contendo um replay, e que deverá
-retornar um dicionário com as seguintes chaves:
-bola - lista contendo tuplos com as coordenadas xx e yy da bola
-jogador_vermelho - lista contendo tuplos com as coordenadas xx e yy da do jogador\_vermelho
-jogador_azul - lista contendo tuplos com as coordenadas xx e yy da do jogador\_azul
-"""
-
-
 def le_replay(nome_ficheiro):
     replay_data = {"bola": [], "jogador_vermelho": [], "jogador_azul": []}
 
     try:
         with open(nome_ficheiro, "r") as file:
-            for linha in file:
-                partes = linha.split()
+            for i, linha in enumerate(file):
+                partes = linha.strip().split(";")
 
-                if len(partes) == 3:
-                    # obter as coordenadas e a cor do objeto
-                    x, y, cor = map(float, partes)
-
-                    # adicionar as coordenadas ao dicionário apropriado
-                    if cor == 0:
-                        replay_data["bola"].append((x, y))
-                    elif cor == 1:
-                        replay_data["jogador_vermelho"].append((x, y))
-                    elif cor == 2:
-                        replay_data["jogador_azul"].append((x, y))
+                for j in range(len(partes)):
+                    # verificar se existem coordenadas
+                    if partes[j]:
+                        coordenadas = tuple(
+                            float(coord) for coord in partes[j].split(",")
+                        )
+                        # primeira linha
+                        if i == 0:
+                            replay_data["bola"].append(coordenadas)
+                        # segunda linha
+                        elif i == 1:
+                            replay_data["jogador_vermelho"].append(coordenadas)
+                        # terceira linha
+                        elif i == 2:
+                            replay_data["jogador_azul"].append(coordenadas)
 
     except FileNotFoundError:
-        print(f"Arquivo não encontrado: {nome_ficheiro}")
+        print(f"Ficheiro '{nome_ficheiro}' não encontrado!")
 
     return replay_data
-
-    pass
 
 
 def main():
     estado_jogo = foosball_alunos.init_state()
     foosball_alunos.setup(estado_jogo, False)
-    replay = le_replay("replay_jogo_jv_2_ja.txt")
+    estado_jogo["janela"].update()
+
+    replay = le_replay("replay_golo_jv_0_ja_1.txt")
+
     for i in range(len(replay["bola"])):
-        estado_jogo["janela"].update()
         estado_jogo["jogador_vermelho"].setpos(replay["jogador_vermelho"][i])
         estado_jogo["jogador_azul"].setpos(replay["jogador_azul"][i])
-        estado_jogo["bola"]["objecto"].setpos(replay["bola"][i])
+        estado_jogo["bola"]["objeto"].setpos(replay["bola"][i])
+
+        estado_jogo["janela"].update()
     estado_jogo["janela"].exitonclick()
 
 
